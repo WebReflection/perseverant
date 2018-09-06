fbff7042b47e42aee2e6c955095cffc990e4b7d8
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-09-06 23:19:57 +0200

    better README & better test

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ Each key will be stored as regular file with serialized data.
 
 By default, everything is kept super simple so whatever value will be saved as JSON.
 
-It is possible to create a new instance with a different serializer, folder, or name.
+It is possible to create a new instance with a different name, folder, or serializer.
 
 
 ### API
 
-This is the meta description of the API.
+Following the **meta description** of the API.
 
 ```js
 const perseverant = require('perseverant');
@@ -23,28 +23,56 @@ const perseverant = require('perseverant');
 // by default, the name of the storage is 'global' but
 // it is highly suggested to use your own project name instead
 const storage = perseverant.createInstance('my-project'):Perseverant
+
+// or ...
 const storage = perseverant.createInstance({
   name,       // by default 'global'
-  folder,     // by default HOME/.perseverant
+  folder,     // by default $HOME/.perseverant
   serializer  // by default JSON
 }):Perseverant
 
 // retrieve a key (read key file)
-storage.getItem(key[, callback]):Promise(value || null)
+storage.getItem(
+  key
+  [,callback(value || null)]
+):Promise<value || null>
 
 // store a key (write key file)
-storage.setItem(key, value[, callback]):Promise(value)
+storage.setItem(
+  key,
+  value
+  [, callback(value)]
+):Promise<value>
 
 // remove a key (unlink key file)
-storage.removeItem(key[, callback]):Promise
+storage.removeItem(
+  key
+  [, callback]
+):Promise
 
 // clear all keys for the named storage (rm -rf folder and its files)
 // WARNING: if you call this on global name, it'll clean it all
 storage.clear([callback]):Promise
 
-// returns the length of keys
-storage.length([callback]):Promise(length)
+// returns the length of keys, from 0 to N
+storage.length([callback(length)]):Promise<length>
 
 // returns all keys
-storage.keys([callback]):Promise(keys)
+storage.keys([callback(keys[])]):Promise<keys[]>
 ```
+
+### Things to consider
+
+This project is not a database replacement, neither a secure way to store credentials, passwords, or any relevant data.
+
+By default, everything is indeed stored as plain JSON, and in a location any other software can reach.
+
+The goal of this project is to provide, specially to NodeJS CLI, a way to persist data in any kind of device.
+
+
+#### Technically speaking
+
+  * each key is converted into its _base64_ counterpart, and its value stored via `JSON.stringify`
+  * if you provide your own `serializer`, you could even encrypt data while storing, but that's up to you, nothing provided by default.
+  * if you provide your own `serializer`, you can also store [recursive data](https://github.com/WebReflection/flatted#flatted) or buffers and binaries, currently not supported in core (to keep it simple) 
+
--- test.js
+++ test.js
@@ -1,32 +1,57 @@
 var fs = require('fs');
 var storage = require('./cjs');
+var invokes = [];
 
 storage
-  .getItem('nope')
+  .getItem('nope', function (value) {
+    invokes.push(value === null);
+    return value;
+  })
   .then(function (value) {
     console.assert(value === null, 'unknown items return null');
-    storage.setItem('yep', 123).then(function (value) {
+    storage.setItem('yep', 123, function (value) {
+      invokes.push(value === 123);
+      return value;
+    }).then(function (value) {
       console.assert(value === 123, 'setItem pass along the value');
       storage
-        .getItem('yep')
+        .getItem('yep', function (value) {
+          invokes.push(value === 123);
+          return value;
+        })
         .then(function (value) {
           console.assert(value === 123, 'setItem did save the value');
-          storage.keys().then(function (keys) {
+          storage.keys(function (keys) {
+            invokes.push(keys.join(',') === 'yep');
+            return keys;
+          }).then(function (keys) {
             console.assert(keys.length === 1, 'there is one key stored');
             console.assert(keys[0] === 'yep', 'the key is correct');
-            storage.removeItem('yep').then(function () {
+            storage.removeItem('yep', function (value) {
+              invokes.push(value === void 0);
+            }).then(function () {
               storage
                 .getItem('yep')
                 .then(function (value) {
                   console.assert(value === null, 'removeItem dropped the key');
-                  storage.length().then(function (value) {
+                  storage.length(function (value) {
+                    invokes.push(value === 0);
+                    return value;
+                  }).then(function (value) {
                     console.assert(value === 0, 'the key is indeed removed');
                     fs.exists(storage.folder, function (result) {
                       console.assert(result, 'the folder is still there');
                       storage.setItem('any', 'value').then(function () {
-                        storage.clear().then(function () {
+                        storage.clear(function (value) {
+                          invokes.push(value === void 0);
+                        }).then(function () {
                           fs.exists(storage.folder, function (result) {
                             console.assert(!result, 'the whole folder has been cleared');
+                            console.assert(
+                              invokes.length === 7 &&
+                              invokes.every(Boolean),
+                              'all callbacks executed OK'
+                            );
                           });
                         });
                       });
